293e93781817114c5e100e024e2f1c46f23881ea
Iain Wood <iain@soulflyer.local>
2011-04-27 20:38:58 +0700

    Added my settings

--- config/bindings.el
+++ config/bindings.el
@@ -1,1 +1,26 @@
 ;; Place personal bindings here
+
+;; Duplicate TextMate's goto line
+;;("\M-l" . goto-line)
+(global-set-key (kbd "M-l") 'goto-line)
+
+;; Function to open and goto indented next line
+(defun open-next-line()
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+(global-set-key (kbd "s-r") 'recentf-open-files)
+(global-set-key (kbd "s-b") 'ibuffer)
+(global-set-key (kbd "s-d") 'ido-dired)
+
+(defvar diw-minor-mode-map (make-keymap) "diw-minor-mode keymap")
+(define-key diw-minor-mode-map  (kbd "C-j") 'backward-char)
+(define-key diw-minor-mode-map  (kbd "C-l") 'forward-char)
+(define-key diw-minor-mode-map  (kbd "C-i") 'previous-line)
+(define-key diw-minor-mode-map  (kbd "C-k") 'next-line)
+(define-key diw-minor-mode-map (kbd "s-<return>") 'open-next-line)
+
+(define-minor-mode diw-minor-mode
+  "A minor mode so that my key settings aren't shadowed by other major/minor modes"
+  t " diw" 'diw-minor-mode-map)
--- config/core.el
+++ config/core.el
@@ -3,8 +3,33 @@
 ;; such as this colour theme (feel free to change it to your own favourite theme)
 
 ;; use blackbored colour theme
-(load-file (concat dotfiles-lib-dir "blackbored.el"))
-(color-theme-blackbored)
+(load-file (concat dotfiles-lib-dir "tg.el"))
+(color-theme-tg)
 
 ;;Or load external files such as this bindings file:
 (load-dotfile "config/bindings.el")
+
+;;Make the fringes narrow
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(fringe-mode (quote (1 . 1)) nil (fringe)))
+
+;;(set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
+(set-frame-parameter (selected-frame) 'alpha '(95 80))
+(add-to-list 'default-frame-alist '(alpha 95 80))
+
+;;Put up the menu bar
+(menu-bar-mode 1)
+
+;;files open in new window
+(setq pop-up-frames t)
+
+;;stop emacs from shitting all over my file system
+;;(setq make-backup-files nil) ; stop creating those backup~ files
+(setq auto-save-default nil) ; stop creating those #autosave# files
+
+;;Keep a recent files list
+(recentf-mode 1)
